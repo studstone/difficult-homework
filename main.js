04f9d84dfa38b7a558d3d6d29d00736f79863642
studstone <nikolas2996@gmail.com>
2022-03-25 22:34:51 +0300

    fix var

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
-let num = 266219;
+const num = 266219;
 console.log(typeof num);
-let splitNum = ("" + num).split("");
+const splitNum = (num).toString().split("");
 console.log(splitNum);
-let productNum = splitNum.reduce((acc, rec) => acc * rec);
+const productNum = splitNum.reduce((acc, rec) => acc * rec);
 console.log(productNum);
-let degreeNum = productNum ** 3;
+const degreeNum = productNum ** 3;
 console.log(String(degreeNum).slice(0, 2));
