e105e51ce1ba215decd5c33839c90baabbae5672
studstone <nikolas2996@gmail.com>
2022-04-10 18:54:14 +0300

    add collection map

--- main.js
+++ main.js
@@ -1,22 +1,41 @@
 'use strict';
-const week = ['Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота', 'Воскресенье'];
-const day = document.getElementById('fordays');
+const week = new Map([
+  [0, 'Понедельник'],
+  [1, 'Вторник'],
+  [2, 'Среда'],
+  [3, 'Четверг'],
+  [4, 'Пятница'],
+  [5, 'Суббота'],
+  [6, 'Воскресенье'],
+]);
+
+
+const div = document.getElementById('fordays');
 const todayDay = new Date();
+let day = +todayDay.getDay() - 1;
+
 
 const days = function () {
-  week.forEach(function (item, i) {
-    let newdiv = document.createElement('div');
-    if (i === +todayDay.getDay() - 1) {
-      newdiv.classList.add('today');
-      newdiv.textContent = week[i];
-    } else if (item == 'Суббота' || item == 'Воскресенье') {
-      newdiv.classList.add('italic');
-      newdiv.textContent = week[i];
-    } else {
-      newdiv.textContent = week[i];
-    }
-    day.appendChild(newdiv);
+  if (day === -1) {
+    day = 6;
 
+  }
+  week.forEach((value, key, map) => {
+    let newdiv = document.createElement('div');
+    console.log(newdiv);
+
+    if (key === day) {
+      newdiv.classList.add('today');
+      newdiv.textContent = value;
+    }
+    if (value == 'Суббота' || value == 'Воскресенье') {
+      newdiv.classList.add('italic');
+      newdiv.textContent = value;
+    } else {
+      newdiv.textContent = value;
+    }
+    div.appendChild(newdiv);
   });
+
 };
 days();
