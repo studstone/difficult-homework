a24b82924953df835f1edf86f959b28f7fd5d331
studstone <nikolas2996@gmail.com>
2022-04-11 19:18:43 +0300

    complete lesson 8

--- main.js
+++ main.js
@@ -1,41 +1,66 @@
 'use strict';
-const week = new Map([
-  [0, 'Понедельник'],
-  [1, 'Вторник'],
-  [2, 'Среда'],
-  [3, 'Четверг'],
-  [4, 'Пятница'],
-  [5, 'Суббота'],
-  [6, 'Воскресенье'],
-]);
+const weekRu = 'Понедельник, Вторник, Среда, Четверг, Пятница, Суббота, Воскресенье'.split(',');
+const monthRu = 'января, февраля, марта, апреля, мая, июня, июля, августа, сентября, октября, ноября, декабря'.split(',');
 
-
-const div = document.getElementById('fordays');
 const todayDay = new Date();
+let year = todayDay.getFullYear();
+let month = monthRu[todayDay.getMonth()];
+let monthNumber = todayDay.getMonth() + 1;
 let day = +todayDay.getDay() - 1;
+let week = weekRu[day];
+let data = todayDay.getDate();
+let hour = todayDay.getHours();
+let minutes = todayDay.getMinutes();
+let seconds = todayDay.getSeconds();
+const fullDate = data + ' ' + month + ' ' + year + ' года ';
+let time = '';
+const divVariantFirst = document.getElementById('div-variant-first');
 
+const declinationTime = function () {
+  if (hour === 1 || hour === 21) {
+    time = hour + ' час ' + minutes + ' минут ' + seconds + ' секунд';
+  }
+  if (hour >= 2 || hour <= 4 || hour === 22 || hour === 23) {
+    time = hour + ' часа ' + minutes + ' минут ' + seconds + ' секунд';
+  }
+  if (hour === 0 || hour >= 5 || hour <= 20) {
+    time = hour + ' часов ' + minutes + ' минут ' + seconds + ' секунд';
+  }
+};
 
 const days = function () {
   if (day === -1) {
     day = 6;
-
+    divVariantFirst.textContent = 'Сегодня ' + week + ', ' + fullDate + ', ' + time;
+  } else {
+    divVariantFirst.textContent = 'Сегодня ' + week + ', ' + fullDate + ', ' + time;
   }
-  week.forEach((value, key, map) => {
-    let newdiv = document.createElement('div');
-    console.log(newdiv);
-
-    if (key === day) {
-      newdiv.classList.add('today');
-      newdiv.textContent = value;
-    }
-    if (value == 'Суббота' || value == 'Воскресенье') {
-      newdiv.classList.add('italic');
-      newdiv.textContent = value;
-    } else {
-      newdiv.textContent = value;
-    }
-    div.appendChild(newdiv);
-  });
-
+  setTimeout(days, 1000);
 };
+
+declinationTime();
 days();
+
+
+const divVariantSecond = document.getElementById('div-variant-second');
+
+const addZero = function () {
+  if (hour <= 9) {
+    hour += '0' + hour;
+  }
+  if (minutes <= 9) {
+    minutes = '0' + minutes;
+  }
+  if (seconds <= 9) {
+    seconds = '0' + seconds;
+  }
+  if (data <= 9) {
+    data = '0' + data;
+  }
+  if (monthNumber <= 9) {
+    monthNumber = '0' + monthNumber;
+  }
+  divVariantSecond.textContent = hour + ':' + minutes + ':' + seconds + ' - ' + data + '.' + monthNumber + '.' + year;
+  // setTimeout(addZero, 1000);
+};
+addZero();
